cce6def9b8d18b05afe4bc54f3186749927c82ad
yusufcanbircan <yusufcannnz@hotmail.com>
2023-03-24 21:55:37 +0300

    detail view viewmodel delegate

--- Rick&MortyApp/ViewModels/RMEpisodeDetailViewViewModel.swift
+++ Rick&MortyApp/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -7,13 +7,28 @@
 
 import Foundation
 
+protocol RMEpisodeDetailViewViewModelDelegate: AnyObject {
+    func didFetchEpisodeDetails()
+}
+
 final class RMEpisodeDetailViewViewModel {
     private let endpointUrl: URL?
+    private var dataTuple: (RMEpisode, [RMCharacter])? {
+        didSet {
+            delegate?.didFetchEpisodeDetails()
+        }
+    }
+    
+    public weak var delegate: RMEpisodeDetailViewViewModelDelegate?
     
     init(endpointUrl: URL?) {
         self.endpointUrl = endpointUrl
         fetchEpisode()
     }
+    
+    // MARK: - Public
+    
+    // MARK: - Private
     
     private func fetchEpisode() {
         guard let url = endpointUrl,
@@ -23,13 +38,46 @@ final class RMEpisodeDetailViewViewModel {
         }
         
         RMService.shared.execute(request,
-                                 expecting: RMEpisode.self) { result in
+                                 expecting: RMEpisode.self) { [weak self] result in
             switch result {
-            case .success(let success):
-            print(String(describing: success))
+            case .success(let model):
+                self?.fetchRelatedCharacters(episode: model)
             case .failure:
                 break
             }
+        }
+    }
+    
+    private func fetchRelatedCharacters(episode: RMEpisode) {
+        
+        let requests: [RMRequest] = episode.characters.compactMap({
+            return URL(string: $0)
+        }).compactMap({
+            return RMRequest(url: $0)
+        })
+        
+        let group = DispatchGroup()
+        var characters: [RMCharacter] = []
+        
+        for request in requests {
+            group.enter()
+            RMService.shared.execute(request,
+                                     expecting: RMCharacter.self) { result in
+                defer {
+                    group.leave()
+                }
+                
+                switch result {
+                case .success(let model):
+                    characters.append(model)
+                case .failure:
+                    break
+                }
+            }
+        }
+        
+        group.notify(queue: .main) {
+            self.dataTuple = (episode, characters)
         }
     }
 }
