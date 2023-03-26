45e8b1f69e968d40d62d39c3887aaf232669c8d7
yusufcanbircan <yusufcannnz@hotmail.com>
2023-03-26 16:50:27 +0300

    swiftui settings view created

---
+++ Rick&MortyApp/Views/Settings-SwiftUI/RMSettingsView.swift
@@ -0,0 +1,47 @@
+//
+//  RMSettingsView.swift
+//  Rick&MortyApp
+//
+//  Created by Yusuf Can Bircan on 26.03.2023.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.white)
+                        .padding(7)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+                
+            }
+            .padding(2)
+        }
+    }
+}
+
+struct RMSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+            return RMSettingsCellViewModel(type: $0)
+        })))
+    }
+}
