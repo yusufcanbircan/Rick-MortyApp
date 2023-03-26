e8abc94115c7f8e4aa4c47e8de3931983f9093a8
yusufcanbircan <yusufcannnz@hotmail.com>
2023-03-26 16:04:50 +0300

    settings viewModels have created

---
+++ Rick&MortyApp/ViewModels/Setting/RMSettingsViewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RMSettingsViewViewModel.swift
+//  Rick&MortyApp
+//
+//  Created by Yusuf Can Bircan on 26.03.2023.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+    
+    
+}
