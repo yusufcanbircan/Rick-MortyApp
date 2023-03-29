f33d7905fb53adfea533d405b07bf37947a42d44
yusufcanbircan <yusufcannnz@hotmail.com>
2023-03-29 15:25:18 +0300

    rmlocationviewviewmodel fetch location setted up

--- Rick&MortyApp/ViewModels/Location/RMLocationViewViewModel.swift
+++ Rick&MortyApp/ViewModels/Location/RMLocationViewViewModel.swift
@@ -9,7 +9,31 @@ import Foundation
 
 final class RMLocationViewViewModel {
     
-    init() {
-        
+    private var locations: [RMLocation] = []
+    private var cellViewModels: [String] = []
+    
+    private var hasMoreResult: Bool {
+        return false
     }
+    
+    // MARK: Init
+    
+    init() {}
+    
+    // MARK: Public
+    
+    public func fetchLocations() {
+        RMService.shared.execute(.listLocationsRequest,
+                                 expecting: String.self) { result in
+            switch result {
+            case .success(let model):
+                break
+            case .failure(let error):
+                break
+            }
+        }
+    }
+    
+    // MARK: Private
+    
 }
